46573a145223173ff356448a8c926f58d1da65aa
Koguchi <koguchic@gmail.com>
2018-05-18 12:33:59 -0700

    Adds minibatch functionality to dataloader file

--- data_loader.py
+++ data_loader.py
@@ -4,26 +4,33 @@ from torch.utils.data import Dataset, DataLoader
 from skimage import io, transform
 from matplotlib import pyplot as plt
 from torch import nn
-
-
+from torchvision.datasets import ImageFolder
+from torchvision import transforms
 
 
 class Data(Dataset):
-    """Face Landmarks dataset."""
 
-    def __init__(self, root_dir, transform=None):
+    def __init__(self, root_dir, batch_size = 128):
+        
         """
         Args:
             root_dir (string): Directory with all the images.
-            transform (callable, optional): Optional transform to be applied
-                on a sample.
-            file_list (list): List of string of ALL FILES in directory
+            file_list (list): List of strings of filename in batch
+            img_list: List of images
         """
+
         self.root_dir = root_dir
-        self.transform = transform
+        self.batch_size = batch_size
+
+        normalize = transforms.Compose([
+             transforms.Scale(256),
+             transforms.ToTensor(),
+             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+             ])
+
+        self.transform = normalize
 
         # Go to root directory and create list of all filenames
-       
         self.file_list = [f for f in os.listdir(root_dir) if os.path.isfile(os.path.join(root_dir, f))]
 
 
@@ -32,115 +39,32 @@ class Data(Dataset):
 
     def __getitem__(self, idx):
 
+        # TODO: Get Transforms working: ERROR Related PIL and numpy transform...
+
         # Fetch file from directory
-        filename = self.file_list[idx]
+        filename = self.file_list[idx:idx+self.batch_size]
 
         # Images stored as Numpy Array (256, 256, 3)
-        img = io.imread(self.root_dir+filename)
+        img_list = [io.imread(self.root_dir+f) for f in filename]
+        # img = io.imread(self.root_dir+filename)
 
         # Store samples as dictionary {String filename, ndarray image}
-        sample = {'filename': filename, 'img': img}
+        # sample = {'filename': filename, 'img': img}
 
-        # Perform transformation
-        if self.transform:
-            sample['img'] = self.transform(sample['img'])
+        # Perform transformatation
+        # img_list = [self.transform(img) for img in img_list]
+        
+        # sample['img'] = self.transform(sample['img'])
 
-        return sample
+        # can return filename too!
+
+        return img_list
 
 def imshow(sample):
 
     plt.title(sample['filename'])
     plt.imshow(sample['img'])
     plt.show()
-
-
-class ConvGenerator(nn.Module):
-    def __init__(self):
-        super(ConvGenerator, self).__init__()
-
-        self.encode = self._encode_layers()
-        self.decode = self._decode_layers()
-
-    def _encode_layers(self):
-        """
-        Generate encode layers for network
-        :return: layers - list of layers describing network architecture
-
-        Note: How to calculate output width after a convolution:
-            Width = (Width - Kernel + 2*Padding) / Stride + 1
-
-        """
-
-        layers = []
-
-        # Input: 256 x 256 x 3   Output: 128 x 128 x 16
-        layers.append(torch.nn.Conv2d(in_channels=3, out_channels=16, kernel_size=4, stride=2, padding=1))
-        layers.append(torch.nn.BatchNorm2d(16))
-        layers.append(torch.nn.LeakyReLU(inplace=True))
-
-        # Input: 128 x 128 x 16   Output: 64 x 64 x 32
-        layers.append(torch.nn.Conv2d(in_channels=16, out_channels=16, kernel_size=4, stride=2, padding=1))
-        layers.append(torch.nn.BatchNorm2d(16))
-        layers.append(torch.nn.LeakyReLU(inplace=True))
-
-        return torch.nn.Sequential(*layers)
-
-    def _decode_layers(self):
-
-        """
-        Generate decode layers for network
-        :return: layers - list of layers describing network architecture
-
-        Note: How to calculate output width after a de-convolution:
-            Width = Stride(Width - 1) + Kernel - 2*Padding
-
-        """
-
-        layers = []
-
-        # Input: 64 x 64 x 32  Output: 128 x 128 x 16
-        layers.append(torch.nn.ConvTranspose2d(in_channels=32, out_channels=16, kernel_size=4, stride=2, padding=1))
-        layers.append(torch.nn.BatchNorm2d(32))
-        layers.append(torch.nn.LeakyReLU(inplace=True))
-
-        # Input: 128 x 128 x 16  Output: 256 x 256 x 3
-        layers.append(torch.nn.ConvTranspose2d(in_channels=16, out_channels=3, kernel_size=4, stride=2, padding=1))
-        layers.append(torch.nn.BatchNorm2d(3))
-        layers.append(torch.nn.LeakyReLU(inplace=True))
-
-        return torch.nn.Sequential(*layers)
-
-
-    def forward(self, inputs):
-        x = inputs
-        x = self.encode(x)
-        x = self.decode(x)
-
-        return x
-
-
-if __name__ == "__main__": 
-
-    G = ConvGenerator()
-
-
-    dataset_dir = '/home/ckoguchi/Documents/ECE 285/Datasets/monet2photo/trainB'
-    landscape_dataset = Data(dataset_dir)
-
-
-    for i in range(len(landscape_dataset)):
-        if i == 1:
-            break
-
-        sample = landscape_dataset[i]
-        # y = G.forward(sample)
-
-        print(sample['img'].shape)
-        print(type(sample['img']))
-
-        imshow(sample)
-
-
 
 
 
